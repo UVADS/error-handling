094d367661021d6ca95f99340c443f0ffd748f10
Neal Magee <nem2p@virginia.edu>
2026-04-28 13:03:15 -0400

    Split aws examples

--- python/aws-sqs-example.py
+++ python/aws-sqs-example.py
@@ -1,40 +1,6 @@
-# The Python interface to the AWS cloud is called boto3
-# https://boto3.amazonaws.com/v1/documentation/api/latest/index.html
-#
-# Boto has an error handling package and bundle of classes.
-#
-# Here is the typical format for exception handling in boto3
-# using the try/except syntax:
-
-
-import botocore
-import boto3
-
-
-def generic_api_call():
-    client = boto3.client('aws_service_name')
-
-    try:
-        client.some_api_call(SomeParam='some_param')
-
-    except botocore.exceptions.ClientError as error:
-        # Put your error handling logic here
-        raise error
-
-    except botocore.exceptions.ParamValidationError as error:
-        raise ValueError('The parameters you provided are incorrect: {}'.format(error))
-
-
-if __name__ == '__main__':
-    generic_api_call()
-
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
 # For a specific service it might look something like this, with
 # output that parses the error response and prints them. Note the
 # print lines are pulling out specific JSON values:
-
 
 import botocore
 import boto3
