1a00ea1c2d9f78f848303ac870a7b70be0f0b66c
Neal Magee <nem2p@virginia.edu>
2026-04-28 13:05:54 -0400

    Combine exceptions and logging

--- python/aws-sqs-example.py
+++ python/aws-sqs-example.py
@@ -1,25 +1,40 @@
 # For a specific service it might look something like this, with
-# output that parses the error response and prints them. Note the
-# print lines are pulling out specific JSON values:
+# output that parses the error response and prints them.
+# Logging is also incorporated here.
 
 import botocore
 import boto3
+import logging
+
+logging.basicConfig(
+    filename='aws-sqs-example.log',
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(message)s',
+)
+logger = logging.getLogger(__name__)
 
 
 def send_sqs_message():
+    logger.info('Starting send_sqs_message')
+
+    logger.info('Creating SQS client')
     client = boto3.client('sqs')
     queue_url = 'SQS_QUEUE_URL'
+    logger.info('Target queue URL: %s', queue_url)
 
     try:
-        client.send_message(QueueUrl=queue_url, MessageBody=('some_message'))
+        logger.info('Sending message to queue')
+        response = client.send_message(QueueUrl=queue_url, MessageBody=('some_message'))
+        logger.info('Message sent successfully. MessageId: %s', response.get('MessageId'))
 
     except botocore.exceptions.ClientError as err:
         if err.response['Error']['Code'] == 'InternalError': # Generic error
             # We grab the message, request ID, and HTTP code to give to customer support
-            print('Error Message: {}'.format(err.response['Error']['Message']))
-            print('Request ID: {}'.format(err.response['ResponseMetadata']['RequestId']))
-            print('Http code: {}'.format(err.response['ResponseMetadata']['HTTPStatusCode']))
+            logger.error('Error Message: %s', err.response['Error']['Message'])
+            logger.error('Request ID: %s', err.response['ResponseMetadata']['RequestId'])
+            logger.error('Http code: %s', err.response['ResponseMetadata']['HTTPStatusCode'])
         else:
+            logger.exception('Unhandled ClientError while sending SQS message')
             raise err
 
 
